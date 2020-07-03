cf4ec217c454125d378fec7d705539096ef4aba0
madzarevic <madzarevic@gmail.com>
2020-07-02 19:09:09 -0700

    Fixed some stuff nad python code

--- sudoku-solver.py
+++ sudoku-solver.py
@@ -1,12 +1,12 @@
 import sys, copy
+from enum import Enum
 
 breadth = 0
 
-#class Board(enum.Enum):
-class State():
-    solved = 0
-    unsolved = 1
-    broken = 2
+class State(Enum):
+    SOLVED = 0
+    UNSOLVED = 1
+    BROKEN = 2
 
 def row(index): return index // 9
 def column(index): return index % 9
@@ -20,12 +20,23 @@ class Square(object):
         self._subgrid = subgrid(index)
         self._value = 0
         self._restrictions = set()
-    
+
+    @property
     def index(self): return self._index    
+ 
+    @property
     def row(self): return self._row
+ 
+    @property
     def column(self): return self._column
+ 
+    @property
     def subgrid(self): return self._subgrid
+ 
+    @property
     def value(self): return self._value
+ 
+    @property
     def branches(self): return 9 - len(self._restrictions)
         
     def possibleValues(self):
@@ -33,39 +44,38 @@ class Square(object):
             if i not in self._restrictions:
                 yield i
     
-    def setValue(self, value):
+    @value.setter
+    def value(self, value):
         if not self.getRestriction(value):
             self._value = value
             self._restrictions = set([1, 2, 3, 4, 5, 6, 7, 8, 9])
             self._restrictions.remove(value)
-            
+     
     def getRestriction(self, value): return value in self._restrictions
+
     def setRestriction(self, value):
-        if self.value() == 0:
+        if self.value == 0:
             self._restrictions.add(value)
 
 class Board(object):
-    def __init__(self, name = "Unnamed"):
-        self.setName(name)
+    def __init__(self, name = 'Unnamed'):
+        self.name = name
         self.clear()
         global breadth
         breadth += 1
         
     def __deepcopy__(self, memo={}):
         boardCopy = Board()
-        boardCopy._name = copy.deepcopy(self._name, memo)
+        boardCopy.name = copy.deepcopy(self.name, memo)
         boardCopy._squares = copy.deepcopy(self._squares, memo)
         return boardCopy
-        
-    def name(self): return self._name
-    def setName(self, name): self._name = name
     
     def clear(self):
         self._squares = list(Square(i) for i in range(81))
         
     def inputEuler(self, lines):
         self.clear()
-        self.setName(lines[0].strip())
+        self.name = lines[0].strip()
         for i in range(9):
             row = lines[i + 1]
             for j in range(9):
@@ -75,58 +85,58 @@ class Board(object):
         return 10
     
     def outputEuler(self):
-        print(self.name() + "\n", end="")
+        print(self.name)
         for i in range(9):
             for j in range(9):
                 square = self._squares[i * 9 + j]
                 value = square.value()
-                print(str(value), end="")
-            print("\n", end="")
+                print(value, end='')
+            print('')
             
     def outputPretty(self):
-        print("+-----------+\n|{:^11}|\n".format(self.name()), end="")
+        print("+-----------+\n|{:^11}|".format(self.name))
         for i in range(9):
             if i % 3 == 0:
-                print("+---+---+---+\n", end="")
+                print('+---+---+---+')
             for j in range(9):
                 if j % 3 == 0:
-                    print("|", end="")
+                    print('|', end='')
                 square = self._squares[i * 9 + j]
-                value = square.value()
-                print(str(value), end="")
-            print("|\n", end="")
-        print("+---+---+---+\n", end="")
+                value = square.value
+                print(value, end='')
+            print('|')
+        print('+---+---+---+')
        
     def setValue(self, index, value):
         square = self._squares[index]
-        row = square.row()
-        column = square.column()
-        subgrid = square.subgrid()
-        square.setValue(value)
+        row = square.row
+        column = square.column
+        subgrid = square.subgrid
+        square.value = value
         for otherSquare in self._squares:
-            if row == otherSquare.row() or column == otherSquare.column() or subgrid == otherSquare.subgrid():
+            if row == otherSquare.row or column == otherSquare.column or subgrid == otherSquare.subgrid:
                 otherSquare.setRestriction(value)
                 
     def evolve(self):
         while True:
             changed = False
             for square in self._squares:
-                if square.branches() == 1 and square.value() == 0:
+                if square.branches == 1 and square.value == 0:
                     possibleValues = list(square.possibleValues())
                     if len(possibleValues) == 1:
-                        self.setValue(square.index(), possibleValues[0])
+                        self.setValue(square.index, possibleValues[0])
                         changed = True
             if not changed:
                 break
             
     def state(self):
-        state = State.solved
+        state = State.SOLVED
         for square in self._squares:
-            branches = square.branches()
+            branches = square.branches
             if branches > 1:
-                state = State.unsolved
+                state = State.UNSOLVED
             elif branches < 1:
-                state = State.broken
+                state = State.BROKEN
                 break
         return state
     
@@ -134,34 +144,34 @@ class Board(object):
         minBranches = 10
         branchSquare = None
         for square in self._squares:
-            if square.value() == 0 and square.branches() < minBranches:
-                minBranches = square.branches()
+            if square.value == 0 and square.branches < minBranches:
+                minBranches = square.branches
                 branchSquare = square
                 if minBranches <= 2:
                     break
         
         for value in branchSquare.possibleValues():
             boardCopy = copy.deepcopy(self)
-            boardCopy.setValue(branchSquare.index(), value)
+            boardCopy.setValue(branchSquare.index, value)
             yield boardCopy
     
     def solve(self, depth = 0):
         self.evolve()
-        if self.state() == State.solved:
-            sys.stderr.write("depth:{}\n".format(depth))
-            sys.stderr.write("breadth:{}\n".format(breadth))
+        if self.state() == State.SOLVED:
+            print("depth:{}".format(depth), file=sys.stderr)
+            print("breadth:{}".format(breadth), file=sys.stderr)
             return self
-        elif self.state() == State.broken:
+        elif self.state() == State.BROKEN:
             return self
         else:
             returnBoard = None
             for child in self.children():
                 returnBoard = child.solve(depth + 1)
-                if returnBoard.state() == State.solved:
+                if returnBoard.state() == State.SOLVED:
                     break
             return returnBoard
     
-    def proof(self): return 100 * self._squares[0].value() + 10 * self._squares[1].value() + self._squares[2].value()
+    def proof(self): return 100 * self._squares[0].value + 10 * self._squares[1].value + self._squares[2].value
         
 def main():
     global breadth
@@ -176,7 +186,7 @@ def main():
         solution.outputPretty()
         sum += solution.proof()
         
-    sys.stderr.write("sum:{}\n".format(sum))
+    print("sum:{}".format(sum), file=sys.stderr)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
